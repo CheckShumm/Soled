8ceb652b9e0ced4f0c5d4fd28093a55ee38d0c54
checkshumm <checkshumm@gmail.com>
2020-07-29 23:59:22 -0400

    Added bottom nav bar

--- mobile/soled/lib/main.dart
+++ mobile/soled/lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
-import 'package:soled/widgets/home.dart';
+import 'package:soled/screens/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    FlutterStatusbarcolor.setStatusBarColor(Colors.blue[200]);
+    FlutterStatusbarcolor.setStatusBarColor(Colors.blue[900]);
     return MaterialApp(
       title: 'SOLED',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: Home(),
     );
